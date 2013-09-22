6169ac312b1f1ed808baf372084999b3ee4d32ab
Victor Espinosa <im_vicoy@hotmail.com>
2013-09-22 18:14:54 -0500

    Inicio del WS Qualitas

---
+++ src/wsPlanet/MyService.java
@@ -0,0 +1,16 @@
+package wsPlanet;
+
+public class MyService {
+	
+	public MyService() {
+	}
+	
+	public boolean sumatoria(String key){
+	
+		boolean process = false;
+		
+		
+		return process;
+	}
+
+}
